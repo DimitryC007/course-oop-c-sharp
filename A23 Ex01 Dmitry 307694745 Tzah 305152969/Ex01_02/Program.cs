87d6b7a900fe1b84c4583a1d9c07bb4b4bf32c0a
Tzach Fleischer <tzahflisher@gmail.com>
2022-11-19 13:32:51 +0200

    Created ex2 and ex3

---
+++ A23 Ex01 Dmitry 307694745 Tzah 305152969/Ex01_02/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Ex01_02
+{
+    public class Program
+    {
+        
+        public static void Main()
+        {
+            int row = 13;
+            PrintDiamond(row);
+
+        }
+
+        public static void PrintDiamond(int i_SizeOfDiamond)
+        {
+            if (i_SizeOfDiamond < 1)
+                return;
+            if (i_SizeOfDiamond == 1)
+            {
+                Console.WriteLine("*");
+                return;
+            }
+            if (i_SizeOfDiamond == 2)
+            {
+                Console.WriteLine("*\n*");
+                return;
+            }
+            int top = (i_SizeOfDiamond / 2) + 1;//3
+            int bottom = i_SizeOfDiamond - top;//2
+            PrintTopTriangle(top, 1);
+            PrintBottomTriangle(bottom, i_SizeOfDiamond - 2, 1);
+
+
+        }
+
+        public static void PrintTopTriangle(int rows,int numOfStars)// 3
+        {
+            int space = rows - 1;
+            if(rows == 0)
+            {
+                return;
+            }
+            PrintSpace(space);
+            PrintStar(numOfStars);
+            Console.WriteLine();
+            PrintTopTriangle(rows - 1, numOfStars + 2);
+        }
+
+        public static void PrintSpace(int space)
+        {
+            if (space < 1)
+                return;
+            Console.Write(" ");
+
+            PrintSpace(space - 1);
+        }
+
+        public static void PrintStar(int numberOfStars)
+        {
+            if (numberOfStars < 1)
+                return;
+            Console.Write("*");
+            PrintStar(numberOfStars - 1);
+        }
+
+        public static void PrintBottomTriangle(int rows,int numOfStars,int space)
+        {
+            if (rows == 0)
+            {
+                return;
+            }
+            PrintSpace(space);
+            PrintStar(numOfStars);
+            Console.WriteLine();
+            PrintBottomTriangle(rows - 1, numOfStars - 2, space + 1);
+        }
+
+
+    }
+}
---
+++ A23 Ex01 Dmitry 307694745 Tzah 305152969/Ex01_03/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Ex01_02;
+
+
+namespace Ex01_03
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int sizeOfDiamond = GetSizeOfDiamond();
+            Ex01_02.Program.PrintDiamond(sizeOfDiamond);
+
+        }
+
+        public static int GetSizeOfDiamond()
+        {
+            Console.Write("Please enter the size of the diamond(number): ");
+            string input = Console.ReadLine();
+            while (!IsValidSizeOfDiamond(input))
+            {
+                Console.Write("Please enter a valid number:");
+                input = Console.ReadLine();
+            }
+            int sizeOfDiamond = int.Parse(input);
+
+
+            return FixedSize(sizeOfDiamond);
+
+        }
+
+        public static bool IsValidSizeOfDiamond(string input)
+        {
+            return int.TryParse(input, out int sizeOfDiamond);
+        }
+
+        public static int FixedSize(int number)
+        {
+            if (number % 2 == 0)
+                return number + 1;
+            else
+                return number;
+        }
+    }
+}
