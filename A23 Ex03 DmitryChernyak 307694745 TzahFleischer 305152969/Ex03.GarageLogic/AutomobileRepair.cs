ef64b9bb7bbad6dd66096bee6f07da15762559a3
Tzach Fleischer <tzahflisher@gmail.com>
2022-12-25 20:53:00 +0200

    created, vehicle, car, motorcycle, truck, engine

---
+++ A23 Ex03 DmitryChernyak 307694745 TzahFleischer 305152969/Ex03.GarageLogic/AutomobileRepair.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+    internal class AutomobileRepair
+    {
+        public string _ownerName;
+        public string _ownerPhone;
+        public VehicleStatus _vehicleStatus;
+        public Vehicle _vehicle;
+    }
+}
---
+++ A23 Ex03 DmitryChernyak 307694745 TzahFleischer 305152969/Ex03.GarageLogic/Truck.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+    internal class Truck : Vehicle
+    {
+        public bool _isDangerousGoods;
+        public int _cargoVolume;
+    }
+}
