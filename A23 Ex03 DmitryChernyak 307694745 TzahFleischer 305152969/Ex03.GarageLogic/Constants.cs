c76721acb18b3c038a954b2986a668beb63bb6a6
DmitryChernyak <dimitryc007@gmail.com>
2022-12-25 20:55:14 +0200

    - added classes

---
+++ A23 Ex03 DmitryChernyak 307694745 TzahFleischer 305152969/Ex03.GarageLogic/Constants.cs
@@ -0,0 +1,49 @@
+﻿namespace Ex03.GarageLogic
+{
+    public enum CarColor
+    {
+        Red,
+        Blue,
+        White,
+        Grey
+    }
+
+    public enum VehicleStatus
+    {
+        UnderRepair,
+        Fixed,
+        Paid
+    }
+
+    public enum VehicleEnergy
+    {
+        Petrol,
+        Electric
+    }
+
+    public enum BikeLicenceType
+    {
+        A,
+        A1,
+        AA,
+        B
+    }
+
+    public enum VehicleType
+    {
+        PetrolCar = 1,
+        ElectricCar = 2,
+        PetrolBike = 3,
+        ElectricBike = 4,
+        PetrolTruck = 5
+    }
+
+    public enum EnergyType
+    {
+        Soler,
+        Octan95,
+        Octan96,
+        Octan98,
+        Electric
+    }
+}
---
+++ A23 Ex03 DmitryChernyak 307694745 TzahFleischer 305152969/Ex03.GarageLogic/VehicleFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ex03.GarageLogic
+{
+    ///TODO: should be a singletone class (design pattern)
+    internal class VehicleFactory
+    {   
+        
+        public object CreateVehicle(VehicleType vehicleType)
+        {
+            throw new NotImplementedException();
+        }
+
+        private object CreateVehicleEnergy(VehicleType vehicleType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
