f27a751c3b74a1f118c223b9397eb982d9288c2c
DmitryChernyak <dimitryc007@gmail.com>
2023-01-27 14:56:26 +0200

    - fixed windows size

--- A23 Ex05 DmitryChernyak 307694745 TzahFleischer 305152969/A23 Ex05 Dmitry 307694745 Tzach 305152969/Ex05_Othelo/GameWindow.cs
+++ A23 Ex05 DmitryChernyak 307694745 TzahFleischer 305152969/A23 Ex05 Dmitry 307694745 Tzach 305152969/Ex05_Othelo/GameWindow.cs
@@ -25,13 +25,13 @@ namespace Ex05_Othelo
 
             InitializeComponent();
             _buttons = new BoardButton[boardSize, boardSize];
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
             InitializeButtons(boardSize);
             gameSettings.Players[0] = new Player("Red", false);
             gameSettings.Players[1] = new Player("Yellow", isComputer);
             _gameLogic = new GameLogic(gameSettings);
             _gameLogic.BoardChanged += GameLogic_BoardChanged;
             _gameLogic.InitGame();
-            this.FormBorderStyle = FormBorderStyle.FixedSingle;
             PlayGame();
         }
 
@@ -76,7 +76,7 @@ namespace Ex05_Othelo
         public void InitializeButtons(int boardSize)
         {
             var size = boardSize * 60;
-            this.Size = new Size(size, size);
+            this.Size = new Size(size, size + 20);
 
             for (int i = 0; i < boardSize; i++)
             {
@@ -84,20 +84,14 @@ namespace Ex05_Othelo
                 {
                     _buttons[i, j] = new BoardButton(new Coordinate(i, j));
                     _buttons[i, j].Size = new Size(50, 50);
-                    _buttons[i, j].Location = new Point(j * 50, i * 50);
+                    _buttons[i, j].Location = new Point(j * 60, i * 60);
                     _buttons[i, j].Click += new EventHandler(button_Click);
                     container.Controls.Add(_buttons[i, j]);
                 }
             }
-            //this.Size = new Size(_buttons[boardSize - 1, boardSize - 1].Location.X, _buttons[boardSize - 1, boardSize - 1].Location.Y);
 
             container.Dock = DockStyle.Fill;
-            //container.Location = this.Location;
-            //this.Size = new Size(boardSize * 50 + 70, boardSize * 50 + 50);
             this.Controls.Add(container);
-
-
-            //this.Size = new Size(container.Size.Width,container.Size.Height);
         }
 
         private void button_Click(object sender, EventArgs e)
